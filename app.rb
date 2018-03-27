31dab932e0dca46c67d76890c175303fbeaa5986
Sean Oughton <seanoughton@gmail.com>
2018-03-27 16:05:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,9 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      
+      params[:pirate][:ships].each do |details|
+      end
+      end
       erb :"pirates/show"
     end
 
